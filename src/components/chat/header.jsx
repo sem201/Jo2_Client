d49686e82d1a473d3f92f3ef0a16fa8df1252c45
sem201 <sem201@naver.com>
2024-08-29 23:26:44 +0900

    feat : 채팅 기능 완성

--- src/components/chat/header.jsx
+++ src/components/chat/header.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import * as S from "./style";
 import back from "../../assets/back.png";
 import apiCallai from "../../api/ApiAi";
+import { useNavigate } from "react-router-dom";
 
 const header=()=>{
+    const navigate=useNavigate()
     const handlewarning =async()=>{
         if (confirm("채팅을 종료하시겠습니까?") == true){
             const response = await apiCallai('/api/chatbot/end', "POST", null);
             console.log('채팅종료',response);
+            navigate('/main');
         }else{  
             return;
         }
