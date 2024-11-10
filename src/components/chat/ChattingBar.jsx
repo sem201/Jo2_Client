ca5a15704db5812788512888516d753be1261322
sem201 <sem201@naver.com>
2024-11-10 20:03:37 +0900

    design : marketDetail 페이지 디자인 완성

--- src/components/chat/ChattingBar.jsx
+++ src/components/chat/ChattingBar.jsx
@@ -8,134 +8,157 @@ import { getToken } from "../../utils/auth";
 import { useReissueToken } from "../../api/ApiReissue";
 
 const ChattingBar = ({ addMessage }) => {
-    const { getReissueToken } = useReissueToken();
-    const textareaRef = useRef(null);
-    const [isRecording, setIsRecording] = useState(false);
-    const [isSending, setIsSending] = useState(false);
-    const [isProcessing, setIsProcessing] = useState(false);
-    const mediaRecorderRef = useRef(null);
-    const audioChunksRef = useRef([]);
+  const { getReissueToken } = useReissueToken();
+  const textareaRef = useRef(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [isSending, setIsSending] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
 
-    const handleReconding = async () => {
-        const token = getToken();
-        if (isProcessing) return;
-        if (!isRecording) {
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-                mediaRecorderRef.current = new MediaRecorder(stream);
-                mediaRecorderRef.current.ondataavailable = (event) => {
-                    audioChunksRef.current.push(event.data);
-                };
-                mediaRecorderRef.current.start();
-                setIsRecording(true);
-            } catch (error) {
-                console.log(error);
-            }
-        } else {
-            mediaRecorderRef.current.stop();
-            mediaRecorderRef.current.onstop = async () => {
-                const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
-                audioChunksRef.current = [];
-                setIsRecording(false);
-                setIsProcessing(true);
+  const handleReconding = async () => {
+    const token = getToken();
+    if (isProcessing) return;
+    if (!isRecording) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+        mediaRecorderRef.current = new MediaRecorder(stream);
+        mediaRecorderRef.current.ondataavailable = (event) => {
+          audioChunksRef.current.push(event.data);
+        };
+        mediaRecorderRef.current.start();
+        setIsRecording(true);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current.onstop = async () => {
+        const audioBlob = new Blob(audioChunksRef.current, {
+          type: "audio/wav",
+        });
+        audioChunksRef.current = [];
+        setIsRecording(false);
+        setIsProcessing(true);
 
-                try {
-                    const formData = new FormData();
-                    formData.append("audio", audioBlob, "recording.wav", "accessToken",token);
-                    console.log("formdata:", formData);
+        try {
+          const formData = new FormData();
+          formData.append(
+            "audio",
+            audioBlob,
+            "recording.wav",
+            "accessToken",
+            token
+          );
+          console.log("formdata:", formData);
 
-                    const response = await apiCallai('/api/chatbot/voice', "POST", formData,token);
-                    const recognizedText = response.data.recognizedText;
-                    const aiResponseMessage = response.data.response.response;
-                    console.log('음성인식 결과:', recognizedText); // 내 음성인식 결과
-                    console.log('응답:', aiResponseMessage); // GPT 응답
+          const response = await apiCallai(
+            "/api/chatbot/voice",
+            "POST",
+            formData,
+            token
+          );
+          const recognizedText = response.data.recognizedText;
+          const aiResponseMessage = response.data.response.response;
+          console.log("음성인식 결과:", recognizedText); // 내 음성인식 결과
+          console.log("응답:", aiResponseMessage); // GPT 응답
 
-                    // 사용자 메시지와 AI 메시지를 상태에 추가
-                    addMessage(recognizedText, 'user');
-                    addMessage(aiResponseMessage, 'ai');
+          // 사용자 메시지와 AI 메시지를 상태에 추가
+          addMessage(recognizedText, "user");
+          addMessage(aiResponseMessage, "ai");
 
-                    // AI 응답을 TTS로 출력
-                    speakText(aiResponseMessage);
-                } catch (error) {
-                    console.log(error);
-                    if(error.response.status === 401){
-                        getReissueToken('/chat') //page마다 다르게
-                    }
-                } finally {
-                    setIsProcessing(false);
-                }
-            };
+          // AI 응답을 TTS로 출력
+          speakText(aiResponseMessage);
+        } catch (error) {
+          console.log(error);
+          if (error.response.status === 401) {
+            getReissueToken("/chat"); //page마다 다르게
+          }
+        } finally {
+          setIsProcessing(false);
         }
-    };
+      };
+    }
+  };
 
-    const speakText = (text) => {
-        if ('speechSynthesis' in window) {
-            const speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'ko-KR';
-            window.speechSynthesis.speak(speech);
-        } else {
-            console.error('TTS 기능을 지원하지 않는 브라우저입니다.');
-        }
-    };
+  const speakText = (text) => {
+    if ("speechSynthesis" in window) {
+      const speech = new SpeechSynthesisUtterance(text);
+      speech.lang = "ko-KR";
+      window.speechSynthesis.speak(speech);
+    } else {
+      console.error("TTS 기능을 지원하지 않는 브라우저입니다.");
+    }
+  };
 
-    const handleResizeHeight = () => {
-        const textarea = textareaRef.current;
-        textarea.style.height = '35px';
-        textarea.style.height = Math.min(textarea.scrollHeight, 60) + 'px';
-    };
+  const handleResizeHeight = () => {
+    const textarea = textareaRef.current;
+    textarea.style.height = "35px";
+    textarea.style.height = Math.min(textarea.scrollHeight, 60) + "px";
+  };
 
-    const sendMessage = async () => {
-        const token = getToken();
-        if (isProcessing) return;
-        const message = textareaRef.current.value;
-        if (message.trim()) {
-            setIsSending(true);
-            setIsProcessing(true);
-            addMessage(message, 'user');
-            textareaRef.current.value = '';
-            textareaRef.current.focus();
-            console.log(textareaRef.current);
-            handleResizeHeight();
-            try {
-                const response = await apiCallai('/api/chatbot/chat', "POST", { "message": message },token);
-                const aiResponseMessage = response.data.response;
-                
-                // AI 메시지를 상태에 추가 및 TTS 실행
-                addMessage(aiResponseMessage, 'ai');
-            } catch (error) {
-                console.log(error);
-                if(error.response.status === 401){
-                    getReissueToken('/chat') //page마다 다르게
-                }
-            } finally {
-                setIsSending(false);
-                setIsProcessing(false);
-            }
+  const sendMessage = async () => {
+    const token = getToken();
+    if (isProcessing) return;
+    const message = textareaRef.current.value;
+    if (message.trim()) {
+      setIsSending(true);
+      setIsProcessing(true);
+      addMessage(message, "user");
+      textareaRef.current.value = "";
+      textareaRef.current.focus();
+      console.log(textareaRef.current);
+      handleResizeHeight();
+      try {
+        const response = await apiCallai(
+          "/api/chatbot/chat",
+          "POST",
+          { message: message },
+          token
+        );
+        const aiResponseMessage = response.data.response;
+
+        // AI 메시지를 상태에 추가 및 TTS 실행
+        addMessage(aiResponseMessage, "ai");
+      } catch (error) {
+        console.log(error);
+        if (error.response.status === 401) {
+          getReissueToken("/chat"); //page마다 다르게
         }
-    };
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault();
-            sendMessage();
-        }
-    };
-    return (
-        <S.ChattingContainer>
-            <S.recordButton onClick={handleReconding} disabled={isSending || isProcessing}>
-                <S.recordimg src={isRecording ? recording : record} />
-            </S.recordButton>
-            <S.messageInput 
-                ref={textareaRef} 
-                onChange={handleResizeHeight}
-                onKeyDown={handleKeyDown}
-                rows="1"
-                disabled={isSending || isProcessing}
-            />
-            <S.Button onClick={sendMessage} disabled={isSending || isProcessing}>
-                <img src={send} alt="send" />
-            </S.Button>
-        </S.ChattingContainer>
-    );
+      } finally {
+        setIsSending(false);
+        setIsProcessing(false);
+      }
+    }
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      sendMessage();
+    }
+  };
+  return (
+    <S.ChattingContainer>
+      <S.recordButton
+        onClick={handleReconding}
+        disabled={isSending || isProcessing}
+      >
+        <S.recordimg src={isRecording ? recording : record} />
+      </S.recordButton>
+      <S.messageInput
+        ref={textareaRef}
+        onChange={handleResizeHeight}
+        onKeyDown={handleKeyDown}
+        rows="1"
+        disabled={isSending || isProcessing}
+      />
+      <S.Button onClick={sendMessage} disabled={isSending || isProcessing}>
+        <img src={send} alt="send" />
+      </S.Button>
+    </S.ChattingContainer>
+  );
 };
 
 export default ChattingBar;
--- src/components/chat/style.jsx
+++ src/components/chat/style.jsx
@@ -1,119 +1,116 @@
 import styled from "styled-components";
 
 export const headerContainer = styled.div`
-    height:80px;
-    width:95%;
-    padding:0 0.5em;
-    display:flex;
-    justify-content: space-between ;
-    align-items: center;
-    position: fixed;
-    top:0;
-    left: 0;
-    z-index: 10
-`
+  height: 80px;
+  width: 95%;
+  padding: 0 0.5em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+`;
 
-export const Img = styled.img`
-    
-`
+export const Img = styled.img``;
 
 export const P = styled.p`
-    font-size: 36px;
-    font-family: Itim;
-`
+  font-size: 36px;
+  font-family: Itim;
+`;
 // chatmain
 export const ChatContainer = styled.div`
-    width:100%;
+  width: 100%;
 
-    margin-top: 50px;
-    max-height:90%;
-    background-color: inherit;
-    overflow-y:auto;
-    &::-webkit-scrollbar {
-        width: 0;
-        background: transparent;
-    }
-`
+  margin-top: 50px;
+  max-height: 90%;
+  background-color: inherit;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+  }
+`;
 
 export const AimessageContainer = styled.div`
-    width:100%;
-    margin-bottom:2em;
-`
+  width: 100%;
+  margin-bottom: 2em;
+`;
 export const UsermessageContainer = styled.div`
-    width:100%;
-    margin-bottom:2em;
-    display: flex;
-    justify-content: flex-end;
-`
+  width: 100%;
+  margin-bottom: 2em;
+  display: flex;
+  justify-content: flex-end;
+`;
 export const chattingAi = styled.div`
-    max-width: 80%;
-    min-height: 30px;
-    background-color: #FFFDF5;
-    border-radius: 30px;
-    padding:10px 15px;
-    display: flex;
-    align-items: center;
-    word-break: break-all;
-    font-size: 18px;
-
-`
+  max-width: 80%;
+  min-height: 30px;
+  background-color: #fffdf5;
+  border-radius: 30px;
+  padding: 10px 15px;
+  display: flex;
+  align-items: center;
+  word-break: break-all;
+  font-size: 18px;
+`;
 
 export const ChattingUser = styled.div`
-    max-width: 80%;
-    min-height: 30px;
-    background-color: #F8DFB8;
-    right:0;
-    border-radius: 30px;
-    padding:10px 15px;
-    display: inline-block;
-    margin-left: 0;
-    align-items: center;
-    justify-content: center;
-    word-break: break-all;
-    font-size: 18px;
-`
+  max-width: 80%;
+  min-height: 30px;
+  background-color: #f8dfb8;
+  right: 0;
+  border-radius: 30px;
+  padding: 10px 15px;
+  display: inline-block;
+  margin-left: 0;
+  align-items: center;
+  justify-content: center;
+  word-break: break-all;
+  font-size: 18px;
+`;
 
 //chattingbar
 export const ChattingContainer = styled.div`
-    width:100vw;
-    min-height:10%;
-    padding:0 0.5em;
-    display: flex;
-    align-items:center;
-    gap:15px;
-    position: fixed;
-    bottom:0;
-    left: 0;
-    background-color: #F8D49E;
-`
+  width: 100vw;
+  min-height: 10%;
+  padding: 0 0.5em;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background-color: #f8d49e;
+`;
 
 export const recordButton = styled.button`
-    width:30px;
-    height: 30px;
-    background-color: inherit;
-    border:0;
-`
+  width: 30px;
+  height: 30px;
+  background-color: inherit;
+  border: 0;
+`;
 export const recordimg = styled.img`
-    width:30px;
-    height: 30px;
-`
+  width: 30px;
+  height: 30px;
+`;
 
 export const messageInput = styled.textarea`
-    box-sizing: border-box;
-    width:70%;
-    height: 35px;
-    padding:0.5em 0.5em;
-    border:0;
-    border-radius: 20px;
-    background-color: #fff;
-    outline: none;
-
-    font-size: 18px;
-`
+  box-sizing: border-box;
+  width: 70%;
+  height: 35px;
+  padding: 0.5em 0.5em;
+  border: 0;
+  border-radius: 20px;
+  background-color: #fff;
+  outline: none;
+  resize: none;
+  font-size: 18px;
+`;
 
 export const Button = styled.button`
-    width: 30px;
-    height: 30px;
-    border:0;
-    background-color: inherit;
-`
+  width: 30px;
+  height: 30px;
+  border: 0;
+  background-color: inherit;
+`;
